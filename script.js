80d08c657eb2ef989f05b855b4f8547a1e068f36
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-05-16 18:12:53 +0200

    New code of insert number function with new variables

--- script.js
+++ script.js
@@ -6,35 +6,55 @@
 var container = document.querySelector(".textview");
 
 var setContainerContents = function (key) {
-    var charFromCode = String.fromCharCode(key);
-    container.textContent = charFromCode;
+	var charFromCode = String.fromCharCode(key);
+	container.textContent = charFromCode;
 }
 var onKeyPress = function (ev) {
-    var keyCode = ev.key;
-    setContainerContents(key);
+	var keyCode = ev.key;
+	setContainerContents(key);
 }
 document.addEventListener("keypress", onKeyPress);
 
 /*--------------------------------------------------
 ----------------------------------------------------*/
 
+var resultValue = "";
+var newValue = "";
+var decimalClicked = false;
 
 function insert(num) {
-    document.form.textview.value = document.form.textview.value + num;
+
+	if (resultValue) {
+		newValue = num;
+		resultValue = "";
+	} else {
+		if (num === '.') {
+			if (decimalClicked != true) {
+				newValue += num;
+				decimalClicked = true;
+			}
+		} else {
+			newValue += num;
+		}
+	}
+	document.form.textview.value = newValue;
 }
 
 function equal() {
-    var exp = document.form.textview.value;
-    if (exp) {
-        document.form.textview.value = eval(exp);
-    }
+	var exp = document.form.textview.value;
+	if (exp) {
+		document.form.textview.value = eval(exp);
+	}
 }
 
 function clean() {
-    document.form.textview.value = "0";
+	newValue = "";
+	resultValue = "";
+	decimalClicked = false;
+	document.form.textview.value = "0";
 }
 
 function back() {
-    var exp = document.form.textview.value;
-    document.form.textview.value = exp.substring(0, exp.length - 1);
+	var exp = document.form.textview.value;
+	document.form.textview.value = exp.substring(0, exp.length - 1);
 }
